c2b47e56492bd9b81e926dcb7966877e40f8a781
Alex Z <alexzabo85@gmail.com>
2020-12-07 13:11:27 +0200

    initial

---
+++ config/config.js
@@ -0,0 +1,17 @@
+const uri = "mongodb+srv://admin:admin@cluster0.io6ba.mongodb.net/crtv?w=majority" //account: alexzabo85@g...com
+
+const config = {
+  env: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 3000,
+  jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+  mongoUri: uri || process.env.MONGODB_URI ||
+    process.env.MONGO_HOST ||
+    'mongodb://' + (process.env.IP || 'localhost') + ':' +
+    (process.env.MONGO_PORT || '27017') +
+    '/mernproject',
+  stripe_connect_test_client_id: 'YOUR_stripe_connect_test_client',
+  stripe_test_secret_key: 'YOUR_stripe_test_secret_key',
+  stripe_test_api_key: 'YOUR_stripe_test_api_key'
+}
+
+export default config
