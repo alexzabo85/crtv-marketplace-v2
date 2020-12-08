e3f67c072a68a4fb405ad6f6d722a48ab51cc341
Alex Z <alexzabo85@gmail.com>
2020-12-08 10:50:44 +0200

    edit style - CartItems.js

--- client/cart/CartItems.js
+++ client/cart/CartItems.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import auth from './../auth/auth-helper'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
@@ -8,12 +8,13 @@ import TextField from '@material-ui/core/TextField'
 import Typography from '@material-ui/core/Typography'
 import Divider from '@material-ui/core/Divider'
 import PropTypes from 'prop-types'
-import {makeStyles} from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles'
 import cart from './cart-helper.js'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const useStyles = makeStyles(theme => ({
   card: {
+    width: 'auto',
     margin: '24px 0px',
     padding: '16px 40px 60px 40px',
     backgroundColor: '#80808017'
@@ -30,14 +31,16 @@ const useStyles = makeStyles(theme => ({
   textField: {
     marginLeft: theme.spacing(1),
     marginRight: theme.spacing(1),
-    marginTop: 0,
-    width: 50
+    marginTop: 12 ,
+    width: 50,
+    fontSize: 'inherit',
   },
   productTitle: {
-    fontSize: '1.15em',
+    fontSize: '2.5rem',
     marginBottom: '5px'
   },
   subheading: {
+    fontSize: '2rem',
     color: 'rgba(88, 114, 128, 0.67)',
     padding: '8px 10px 0',
     cursor: 'pointer',
@@ -79,7 +82,7 @@ const useStyles = makeStyles(theme => ({
     verticalAlign: 'bottom'
   },
   continueBtn: {
-    marginLeft: '10px'
+    marginRight: '10px'
   },
   itemShop: {
     display: 'block',
@@ -87,19 +90,20 @@ const useStyles = makeStyles(theme => ({
     color: '#78948f'
   },
   removeButton: {
-    fontSize: '0.8em'
+    fontSize: '0.8em',
+    marginBottom: '8px'
   }
 }))
 
-export default function CartItems (props) {
+export default function CartItems(props) {
   const classes = useStyles()
   const [cartItems, setCartItems] = useState(cart.getCart())
 
   const handleChange = index => event => {
     let updatedCartItems = cartItems
-    if(event.target.value == 0){
+    if (event.target.value == 0) {
       updatedCartItems[index].quantity = 1
-    }else{
+    } else {
       updatedCartItems[index].quantity = event.target.value
     }
     setCartItems([...updatedCartItems])
@@ -108,13 +112,13 @@ export default function CartItems (props) {
 
   const getTotal = () => {
     return cartItems.reduce((a, b) => {
-        return a + (b.quantity*b.product.price)
+      return a + (b.quantity * b.product.price)
     }, 0)
   }
 
-  const removeItem = index => event =>{
+  const removeItem = index => event => {
     let updatedCartItems = cart.removeItem(index)
-    if(updatedCartItems.length == 0){
+    if (updatedCartItems.length == 0) {
       props.setCheckout(false)
     }
     setCartItems(updatedCartItems)
@@ -124,63 +128,69 @@ export default function CartItems (props) {
     props.setCheckout(true)
   }
 
-    return (<Card className={classes.card}>
-      <Typography type="title" className={classes.title}>
+  return (<Card className={classes.card}>
+    {/* <Typography type="title" className={classes.title}>
         Shopping Cart
-      </Typography>
-      {cartItems.length>0 ? (<span>
-          {cartItems.map((item, i) => {
-            return <span key={i}><Card className={classes.cart}>
-              <CardMedia
-                className={classes.cover}
-                image={'/api/product/image/'+item.product._id}
-                title={item.product.name}
+      </Typography> */}
+    {cartItems.length > 0 ? (<span>
+      {cartItems.map((item, i) => {
+        return <span key={i}><Card className={classes.cart}>
+          <CardMedia
+            className={classes.cover}
+            image={'/api/product/image/' + item.product._id}
+            title={item.product.name}
+          />
+          <div className={classes.details}>
+            <CardContent className={classes.content}>
+              {/* <Link to={'/product/' + item.product._id}> */}
+              <Typography type="title" component="h3" className={classes.productTitle} color="primary">{item.product.name}</Typography>
+              {/* </Link> */}
+              {/* <div>
+                <Typography type="subheading" component="h3" className={classes.price} color="primary">$ {item.product.price}</Typography>
+                <span className={classes.itemTotal}>${item.product.price * item.quantity}</span>
+                <span className={classes.itemShop}>Shop: {item.product.shop.name}</span>
+              </div> */}
+            </CardContent>
+            <div
+              className={classes.subheading}
+            >כמות:<TextField
+                className={classes.textField}
+                value={item.quantity}
+                onChange={handleChange(i)}
+                type="number"
+                inputProps={{
+                  min: 1
+                }}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                margin="normal"
               />
-              <div className={classes.details}>
-                <CardContent className={classes.content}>
-                  <Link to={'/product/'+item.product._id}><Typography type="title" component="h3" className={classes.productTitle} color="primary">{item.product.name}</Typography></Link>
-                  <div>
-                    <Typography type="subheading" component="h3" className={classes.price} color="primary">$ {item.product.price}</Typography>
-                    <span className={classes.itemTotal}>${item.product.price * item.quantity}</span>
-                    <span className={classes.itemShop}>Shop: {item.product.shop.name}</span>
-                  </div>
-                </CardContent>
-                <div className={classes.subheading}>
-                  Quantity: <TextField
-                              value={item.quantity}
-                              onChange={handleChange(i)}
-                              type="number"
-                              inputProps={{
-                                  min:1
-                              }}
-                              className={classes.textField}
-                              InputLabelProps={{
-                                shrink: true,
-                              }}
-                              margin="normal"/>
-                            <Button className={classes.removeButton} color="primary" onClick={removeItem(i)}>x Remove</Button>
-                </div>
-              </div>
-            </Card>
-            <Divider/>
-          </span>})
-        }
-        <div className={classes.checkout}>
-          <span className={classes.total}>Total: ${getTotal()}</span>
-          {!props.checkout && (auth.isAuthenticated()?
-            <Button color="secondary" variant="contained" onClick={openCheckout}>Checkout</Button>
-            :
-            <Link to="/signin">
-              <Button color="primary" variant="contained">Sign in to checkout</Button>
-            </Link>)}
-          <Link to='/' className={classes.continueBtn}>
-            <Button variant="contained">Continue Shopping</Button>
+              <Button className={classes.removeButton} color="primary" onClick={removeItem(i)}>[הסר]</Button>
+            </div>
+          </div>
+        </Card>
+          <Divider />
+        </span>
+      })
+      }
+      <div className={classes.checkout}>
+        {/* <span className={classes.total}>סכום כולל: ₪{getTotal()}</span> */}
+        {!props.checkout && (auth.isAuthenticated() ?
+          <Button color="secondary" variant="contained" onClick={openCheckout}>לקופה</Button>
+          :
+          <Link to="/signin">
+            <Button color="primary" variant="contained">התחבר</Button>
           </Link>
-        </div>
-      </span>) :
-      <Typography variant="subtitle1" component="h3" color="primary">No items added to your cart.</Typography>
+        )}
+        <Link to='/' className={classes.continueBtn}>
+          <Button variant="contained">חזרה לחנות</Button>
+        </Link>
+      </div>
+    </span>) :
+      <Typography variant="subtitle1" component="h3" color="primary">העגלה ריקה.</Typography>
     }
-    </Card>)
+  </Card>)
 }
 
 CartItems.propTypes = {
