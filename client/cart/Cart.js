a3c6e67eae2b94796ed405f7d093d592e0a3a136
Alex Z <alexzabo85@gmail.com>
2020-12-08 10:49:39 +0200

    width: 'auto', for root component - Cart.js

--- client/cart/Cart.js
+++ client/cart/Cart.js
@@ -1,19 +1,20 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import Grid from '@material-ui/core/Grid'
-import {makeStyles} from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles'
 import CartItems from './CartItems'
-import {StripeProvider} from 'react-stripe-elements'
+import { StripeProvider } from 'react-stripe-elements'
 import config from './../../config/config'
 import Checkout from './Checkout'
 
 const useStyles = makeStyles(theme => ({
   root: {
+    width: 'auto',
     flexGrow: 1,
     margin: 30,
   }
 }))
 
-export default function Cart () {
+export default function Cart() {
   const classes = useStyles()
   const [checkout, setCheckout] = useState(false)
 
@@ -21,18 +22,18 @@ export default function Cart () {
     setCheckout(val)
   }
 
-    return (<div className={classes.root}>
-      <Grid container spacing={8}>
-        <Grid item xs={6} sm={6}>
-          <CartItems checkout={checkout}
-                     setCheckout={showCheckout}/>
-        </Grid>
-        {checkout &&
-          <Grid item xs={6} sm={6}>
-            <StripeProvider apiKey={config.stripe_test_api_key}>
-              <Checkout/>
-            </StripeProvider>
-          </Grid>}
-        </Grid>
-      </div>)
+  return (<div className={classes.root}>
+    <Grid container spacing={8}>
+      <Grid item xs={12} >
+        <CartItems checkout={checkout}
+          setCheckout={showCheckout} />
+      </Grid>
+      {checkout &&
+        <Grid item xs={12} >
+          <StripeProvider apiKey={config.stripe_test_api_key}>
+            <Checkout />
+          </StripeProvider>
+        </Grid>}
+    </Grid>
+  </div>)
 }
