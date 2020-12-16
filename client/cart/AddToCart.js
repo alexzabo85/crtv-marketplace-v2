c479a3e6b9395a41ff76435830381daffcca816d
Alex Z <alexzabo85@gmail.com>
2020-12-16 12:39:22 +0200

    use local css style AddToCart.js

--- client/cart/AddToCart.js
+++ client/cart/AddToCart.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import PropTypes from 'prop-types'
 import IconButton from '@material-ui/core/IconButton'
@@ -9,37 +9,96 @@ import { Redirect } from 'react-router-dom'
 
 const useStyles = makeStyles(theme => ({
   iconButton: {
-    width: '28px',
-    height: '28px'
+    width: 50,
+    height: 50,
+    padding: '10px 12px',
+    borderRadius: '0.25em',
+    // backgroundColor: '#5f7c8b',
+    // color: '#b2ff59',
+    backgroundColor: '#4a10a3',
   },
-  disabledIconButton: {
-    color: '#7f7563',
-    width: '28px',
-    height: '28px'
+  iconButtonActive: {
+    width: 50,
+    height: 50,
+    padding: '10px 12px',
+    borderRadius: '0.25em',
+    backgroundColor: '#5f7c8b',
+    // color: '#7f7563',
+    // backgroundColor: '#b2ff59',
+  },
+  addCart: {
+    // float: 'right',
+    width: 50,
+    height: 50,
+    padding: '10px 12px',
+    borderRadius: '0.25em',
+    backgroundColor: '#5f7c8b',
+    // zIndex: 2500
   }
 }))
 
 export default function AddToCart(props) {
   const classes = useStyles()
-  const [redirect, setRedirect] = useState(false)
+  // const [redirect, setRedirect] = useState(false)
+  const redirect = false;
+  const [index, setIndex] = useState(cart.findById(props.item._id))
+  // const [found, setFound] = useState()
+
+  useEffect(() => {
+    setIndex(cart.findById(props.item._id))
+    console.log('[01]index:' + index)
+    // cart.findById(props.item._id)
+    // alert('[01]index:' + index)
+  }, [])
 
   const addToCart = () => {
-    cart.addItem(props.item, () => {
-      setRedirect({ redirect: true })
-    })
+    // alert('[02]index:' + index)
+    if (+index >= 0) {
+      cart.increasQuantity(index)
+      // setRedirect({ redirect: true })
+      setIndex(index)
+    }
+    else {
+      cart.addItem(props.item,
+        () => {
+          const index = cart.findById(props.item._id)
+          setIndex(index)
+          // setRedirect({ redirect: true })
+        }
+      )
+    }
+    redirect = true;
   }
+
+  const removeFromCart = () => {
+    // alert('[03]index:' + index)
+    cart.removeItem(index)
+    // setRedirect({ redirect: true })
+    setIndex(-1)
+    redirect = true;
+
+  }
+
   if (redirect) {
-    return (<Redirect to={'/cart'} />)
+    return (<Redirect to={'/'} />)
+    // window.location.reload(false);
+
   }
-  return (<span>
-    {props.item.quantity >= 0 ?
-      <IconButton color="secondary" dense="dense" onClick={addToCart}>
-        <AddCartIcon className={props.cartStyle || classes.iconButton} />
-      </IconButton> :
-      <IconButton disabled={true} color="secondary" dense="dense">
-        <DisabledCartIcon className={props.cartStyle || classes.disabledIconButton} />
-      </IconButton>}
-  </span>)
+  // const found = cart.findById(props.item._id);
+  return (
+    <span>{
+      (index == -1) ? (
+        <IconButton color="secondary" dense="dense" onClick={addToCart}>
+          <AddCartIcon
+            className={classes.iconButton} />
+        </IconButton>
+      ) : (
+          <IconButton color="secondary" dense="dense" onClick={removeFromCart}>
+            <DisabledCartIcon className={classes.iconButtonActive} />
+          </IconButton>
+        )
+    }</span>
+  )
 }
 
 AddToCart.propTypes = {
